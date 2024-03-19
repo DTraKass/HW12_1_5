c88763c6da9ba767ed3654c01df534807382d75d
samsung <demonov03@gmail.com>
2024-03-19 17:13:51 +0300

    req

---
+++ Alg/Program.cs
@@ -0,0 +1,42 @@
+﻿using Alg.Users;
+
+namespace Alg
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Привет, {UserData().Name}");
+
+            if (!UserData().IsPremium)
+            {
+                ShowAds();
+            }
+        }
+        static void GetUser()
+        {
+            User user = UserData();
+        }
+
+        static User UserData()
+        {
+            return new User()
+            {
+                Login = "German123",
+                Name = "German",
+                IsPremium = false
+            };
+        }
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            Thread.Sleep(3000);
+        }
+    }
+}
